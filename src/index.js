356d7a93bde148eebcf8f478ef47f6aa0e369473
ahmadsharaf99 <ahmadsharafudeen98@gmail.com>
2023-09-23 18:14:50 +0100

    refactored the searchField to use Redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./containers/App";
+import { Provider } from "react-redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 import reportWebVitals from "./reportWebVitals";
+import { searchRobots } from "./reducers";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger({});
+
+const store = legacy_createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
